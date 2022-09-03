14da08539a1b42724a0d0f6979b5402243b25876
Josh Marshall <catchjosh@gmail.com>
2022-09-03 19:23:31 +0900

    Adding pyproject.toml and simplifying setup as a dependency.

--- setup.py
+++ setup.py
@@ -1,13 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='taming-transformers',
-    version='0.0.1',
-    description='Taming Transformers for High-Resolution Image Synthesis',
-    packages=find_packages(),
-    install_requires=[
-        'torch',
-        'numpy',
-        'tqdm',
-    ],
-)
+from setuptools import setup
+setup()
